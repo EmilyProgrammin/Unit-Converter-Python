a6390a0db7960382a3fe249e99a40db9d6aa2ff2
Emily <106826702+EmilyProgrammin@users.noreply.github.com>
2022-06-05 18:00:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# This is just to show who would contribute, the real files would be programmed on your machine and then uploaded here.
+# Just type (print("placeholder")) to ensure you enter the contribution
+
 print("placeholder")
 
 print("placeholder")
